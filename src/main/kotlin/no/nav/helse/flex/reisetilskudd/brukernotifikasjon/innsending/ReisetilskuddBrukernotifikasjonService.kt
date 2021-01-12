d66864eb840751c7f7d36e7a58a66d8329fe5a0c
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-01-12 15:41:46 +0100

    Compilerbart

--- src/main/kotlin/no/nav/helse/flex/reisetilskudd/brukernotifikasjon/innsending/ReisetilskuddBrukernotifikasjonService.kt
+++ src/main/kotlin/no/nav/helse/flex/reisetilskudd/brukernotifikasjon/innsending/ReisetilskuddBrukernotifikasjonService.kt
@@ -2,7 +2,6 @@ package no.nav.helse.flex.reisetilskudd.brukernotifikasjon.innsending
 
 import no.nav.helse.flex.reisetilskudd.brukernotifikasjon.domain.tilReisetilskudd
 import no.nav.helse.flex.reisetilskudd.brukernotifikasjon.log
-import no.nav.helse.flex.reisetilskudd.gsak.domain.*
 import org.springframework.stereotype.Component
 
 @Component
